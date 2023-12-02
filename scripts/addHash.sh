7d3afd2686b482e9ddb3174c4bc2cccd4ee2b399
Keith Brooks <keith1225@outlook.com>
2023-12-01 20:48:19 -0500

    add an integrity attribute to the demo page's script

--- scripts/addHash.sh
+++ scripts/addHash.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 VERSION=$(jq -r .version < package.json)
 HASH=$(openssl dgst -sha384 -binary dist/index.js | openssl base64 -A | xargs -n 1 -I {} echo "sha384-{}")
-sed -i "" -E -e "s|integrity=".*"|integrity=\"$HASH\"|" ./README.md
+sed -i "" -E -e "s|integrity=".*"|integrity=\"$HASH\"|" ./README.md ./index.html
 git add .
 git commit -m "Updated the integrity hash for version $VERSION"
